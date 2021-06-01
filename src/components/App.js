4d06c4be199172b24291df52d8755d65a65be267
rohitb <rbhogal23@gmail.com>
2021-06-01 12:11:53 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
+class App extends Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language: language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language: &nbsp;
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        <LanguageContext.Provider value={this.state.language} >
+        <UserCreate />
+        </LanguageContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
