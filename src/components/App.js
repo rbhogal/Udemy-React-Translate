b53765b6955822213980048ad3edfd93f2517869
rohitb <rbhogal23@gmail.com>
2021-06-01 15:49:54 -0700

    course finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends Component {
   state = { language: 'english' };
 
-  onLanguageChange = language => {
-    this.setState({ language: language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language: &nbsp;
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange('english')}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange('dutch')}
-          />
-        </div>
-        <LanguageContext.Provider value={this.state.language} >
-        <UserCreate />
-        </LanguageContext.Provider>
+        <LanguageStore>
+          <LanguageSelector />
+
+          <ColorContext.Provider value="red">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends Component {
-  static contextType = LanguageContext;
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
 
